41c807a7733b4776453a536601acf07844d2b104
Slava Vishnyakov <bomboze@gmail.com>
2020-11-18 22:37:40 +0300

    Update serve.py

--- serve.py
+++ serve.py
@@ -2,4 +2,4 @@ import os
 import waitress
 import web
 
-waitress.serve(web.app, port=int(os.environ.get('PORT', 5000)), url_scheme='http', threads=25)
+waitress.serve(web.app, port=int(os.environ.get('PORT', 5000)), url_scheme='http', threads=25, asyncore_use_poll=True)
