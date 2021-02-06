cba7dd7274bedca31e1b64a558e4ad9dd7ed6961
Slava <bomboze@gmail.com>
2021-02-06 12:42:28 +0300

    Initial

--- init.sh
+++ init.sh
@@ -1,14 +1,16 @@
-```sh
+#!/bin/bash
+
+set -euo pipefail
+
 echo -n "Directory name: "; read PROJECT
 git clone git@github.com:slava-vishnyakov/python-caprover $PROJECT
 cd $PROJECT
 git checkout orator
 rm -rf .git
 python3 initialize.py
-rm initialize.py
+rm initialize.py init.sh
 python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
 npm run start-db &
 npm run serve
-```
